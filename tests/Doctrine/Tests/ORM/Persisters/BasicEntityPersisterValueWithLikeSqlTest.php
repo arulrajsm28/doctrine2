4de928358a9b927895739ba9175ea13c06c2a717
mike <mike.simonson@gmail.com>
2017-01-29 22:52:02 +0100

    Removing detach and merge from the entity manager
    and the related tests

---
+++ tests/Doctrine/Tests/ORM/Persisters/BasicEntityPersisterValueWithLikeSqlTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 4/27/16
+ * Time: 9:35 PM
+ */
+
+namespace Doctrine\Tests\ORM\Persisters;
+
+
+class BasicEntityPersisterValueWithLikeSqlTest
+{
+
+}
